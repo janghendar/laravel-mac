0f5bea77131bd2b573955c553cb298cd13ddb058
smkn1rongga <smkn1rongga@iMac-smkn1rongga.local>
2020-11-05 15:23:12 +0700

    Menambah fungsi di Route

--- resources/views/universitas/mahasiswa.blade.php
+++ resources/views/universitas/mahasiswa.blade.php
@@ -8,22 +8,33 @@
 <body>
     <h1>Daftar Mahasiswa</h1>
     <ol>
-<!--         
-        <li>Suhendar Aryadi</li>
+        
+        <!-- <li>Suhendar Aryadi</li>
         <li>Cici Alifa</li>
         <li>Burhan</li>
         <li>Wiwi Marwiyah</li> -->
 
-        <?php 
+        <?php
+
             foreach ($mahasiswa as $nama) {
                 echo "<li> $nama </li>";
             }
-        
         
         ?>
         
     </ol>
 
+    <!--  echo "$mahasiswa01"; ?> -->
+
+    <!-- <ul>
+    <li>Nama: < echo $nama; ?></li>
+    <li>Umur: < $umur; ?> </li>
+    <li>Kota Asal: < echo $kotaAsal; ?></li> -->
+    
+    
+    </ul>
+    
+    <p>
     <div>
         Copyright <?php echo date("M-Y"); ?>. Suhendar Aryadi
     </div>
